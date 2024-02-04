847d24cd0b8ae06eb24cc988fe10c2d81b91368d
Juan <juanangelbasgall@hotmail.com>
2024-02-04 07:59:16 -0300

    Vistos templates

--- core/views.py
+++ core/views.py
@@ -3,7 +3,14 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hola mundo")
+    contexto = {
+        'titulo' : 'Bienvenidos a la prueba de mi página web con django, ahora dinámica',
+        'nombre_usuario' : 'Pepito Manzanero',
+        
+    }
+    return render(request, 'index.html', contexto)
+
+
 
 def listar_alumnos(request):
     alumnos = ["Carlos", "Maria", "Jose", "Daniela"]
